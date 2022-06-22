4cfcd1dcb86dd2f5dda903a871f64877d9ed70c0
roger <romachad@users.noreply.github.com>
2022-06-22 08:26:29 +0200

    Finally ft_split passed all tests.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 02:35:00 by romachad          #+#    #+#             */
-/*   Updated: 2022/06/19 05:50:28 by romachad         ###   ########.fr       */
+/*   Updated: 2022/06/22 08:11:58 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,23 +17,21 @@ static size_t	count_char(char const *s, char c)
 	size_t	count;
 
 	i = 0;
-	while (s[i] == c)
+	while (s[i] == c && *s)
 		i++;
 	count = 0;
 	while (s[i] != 0 && i < ft_strlen(s))
 	{
-		while (s[i] == c && s[i + 1] == s[i])
+		count++;
+		while (s[i] && s[i] != c)
 			i++;
-		if (s[i] == c && s[i + 1] != 0)
-		{
-			count++;
-		}
-		i++;
+		while (s[i] && s[i] == c)
+			i++;
 	}
 	return (count);
 }
 
-static void	free_array(char **array, size_t i)
+static void	*free_array(char **array, size_t i)
 {
 	while (i > 0)
 	{
@@ -42,6 +40,7 @@ static void	free_array(char **array, size_t i)
 	}
 	free(array[i]);
 	free(array);
+	return (0);
 }
 
 char	**make_array(char **array, size_t count, const char *s, char c)
@@ -53,7 +52,7 @@ char	**make_array(char **array, size_t count, const char *s, char c)
 	i = 0;
 	j = 0;
 	size = 0;
-	while (s[i] != 0 && i < ft_strlen(s) && j < count + 1)
+	while (s[i] != 0 && i < ft_strlen(s) && j < count)
 	{
 		while (s[i] == c && s[i] != 0)
 			i++;
@@ -64,7 +63,7 @@ char	**make_array(char **array, size_t count, const char *s, char c)
 		}
 		array[j] = ft_substr(&s[i - size], 0, size);
 		if (array[j] == 0)
-			free_array(array, j);
+			return (free_array(array, j));
 		size = 0;
 		j++;
 	}
@@ -76,12 +75,23 @@ char	**ft_split(char const *s, char c)
 	size_t	count;
 	char	**array;
 
-	if (s[0] == 0 || c == 0)
+	if (s == 0 || !ft_isascii(c))
 		return (0);
 	count = count_char(s, c);
-	array = (char **) malloc((count + 1) * sizeof(char *));
+	if (count > 0)
+		array = (char **) malloc((count + 1) * sizeof(char *));
+	else
+	{
+		array = (char **) malloc(1 * sizeof(char *));
+		array[0] = 0;
+		return (array);
+	}
 	if (!array)
 		return (0);
 	else
-		return (make_array(array, count, s, c));
+	{
+		array = make_array(array, count, s, c);
+		array[count] = 0;
+		return (array);
+	}
 }
