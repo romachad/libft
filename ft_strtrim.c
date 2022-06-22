0bf2f6982186174ee391fd3bc5f4e71e0681882e
roger <romachad@users.noreply.github.com>
2022-06-22 05:06:36 +0200

    Remade substr, adjusted strtrim. All based on libftTester from tripouille. Possible will need to change all returns to NULL istead of zero, since NULL possible will be freed whereas 0 is cannot.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 21:15:48 by romachad          #+#    #+#             */
-/*   Updated: 2022/06/12 22:46:54 by romachad         ###   ########.fr       */
+/*   Updated: 2022/06/22 04:56:21 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,14 +18,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	full_size = ft_strlen(s1);
 	if (!full_size)
-		return (0);
+		return (ft_calloc(1, sizeof(char)));
 	if (!(ft_strlen(set)))
 		return (ft_strdup(s1));
 	i = 0;
 	while (ft_strchr(set, s1[i]) && s1[i])
 		i++;
 	if (i == full_size)
-		return (0);
+		return (ft_calloc(1, sizeof(char)));
 	while (ft_strrchr(set, s1[full_size]))
 		full_size--;
 	return (ft_substr(s1, i, (full_size - i + 1)));
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 05:55:29 by romachad          #+#    #+#             */
-/*   Updated: 2022/06/16 02:38:17 by romachad         ###   ########.fr       */
+/*   Updated: 2022/06/22 04:23:25 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,19 +14,22 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
-	size_t	i;
+	size_t	s_len;
 
-	if (!len)
+	s_len = ft_strlen(s);
+	if (!*s)
 		return (0);
-	substr = malloc((len + 1) * sizeof(char));
-	if (!substr)
+	if (start > s_len || len == 0)
+		return (ft_calloc(1, sizeof(char)));
+	if (s_len >= len + start)
+		substr = malloc((len + 1) * sizeof(char));
+	else
+		substr = malloc((s_len - start + 1) * sizeof(char));
+	if (substr == 0)
 		return (0);
-	i = 0;
-	while (s[start] != 0 && i < len)
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	substr[i] = 0;
+	else if (substr && s_len >= len + start)
+		ft_strlcpy(substr, &s[start], len + 1);
+	else
+		ft_strlcpy(substr, &s[start], (s_len - start + 1));
 	return (substr);
 }
