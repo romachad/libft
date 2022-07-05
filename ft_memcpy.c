131662b6cc83c07fac13ba50f18f82aa5d84368f
roger <romachad@users.noreply.github.com>
2022-07-05 06:13:22 +0200

    Rolled back memcpy to a less secure version (that molli approves) and refurbished substr after another core dump.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 07:08:15 by romachad          #+#    #+#             */
-/*   Updated: 2022/06/29 02:36:46 by romachad         ###   ########.fr       */
+/*   Updated: 2022/07/04 01:34:03 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char	*dest2;
 	char	*src2;
 
-	if (!src || !dest)
-		return (NULL);
 	dest2 = (char *) dest;
 	src2 = (char *) src;
 	i = 0;
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 05:55:29 by romachad          #+#    #+#             */
-/*   Updated: 2022/06/29 03:05:49 by romachad         ###   ########.fr       */
+/*   Updated: 2022/07/04 02:13:42 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,23 +15,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	s_len;
+	size_t	i;
+	size_t	j;
 
 	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
-	if (!*s)
-		return (0);
 	if (start > s_len || len == 0)
 		return (ft_calloc(1, sizeof(char)));
-	if (s_len >= len + start)
-		substr = malloc((len + 1) * sizeof(char));
-	else
-		substr = malloc((s_len - start + 1) * sizeof(char));
+	if (len > s_len - start)
+		len = s_len - start;
+	substr = ft_calloc((len + 1), sizeof(char));
 	if (substr == 0)
 		return (0);
-	else if (substr && s_len >= len + start)
-		ft_strlcpy(substr, &s[start], len + 1);
-	else
-		ft_strlcpy(substr, &s[start], (s_len - start + 1));
+	i = 0;
+	j = 0;
+	while (s[i + start] && j < len)
+	{
+		substr[j] = s[i + start];
+		j++;
+		i++;
+	}
 	return (substr);
 }
